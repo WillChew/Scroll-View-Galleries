e156e71e90f5cc294c624e34760fcd021da3702a
WillChew <williamchew95@gmail.com>
2018-06-18 11:03:51 -0400

    Initial Commit

---
+++ Scroll View Image Galleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scroll View Image Galleries
+//
+//  Created by Will Chew on 2018-06-18.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
